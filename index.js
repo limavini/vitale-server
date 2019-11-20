832a38498f7e8fa6d059b4d2d1b614502b5ac00c
limavini <vinicius.lalmeida98@gmail.com>
2019-11-20 17:25:51 -0300

    Heroku deploy

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
 const express = require("express");
-var auth = require("./routes/auth");
-require("./passport");
-const graphqlHTTP = require("express-graphql");
-const schema = require("./schema/schema");
-const mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-const cors = require("cors");
-require('dotenv').config();
+// var auth = require("./routes/auth");
+// require("./passport");
+// const graphqlHTTP = require("express-graphql");
+// const schema = require("./schema/schema");
+// const mongoose = require("mongoose");
+// var bodyParser = require("body-parser");
+// const cors = require("cors");
+//require('dotenv').config();
 
 const app = express();
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-app.use(bodyParser.json());
-// permite a conexao com outro servidor
-app.use(cors());
+// const port = process.env.PORT || 8080;
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true
+//   })
+// );
+// app.use(bodyParser.json());
+// // permite a conexao com outro servidor
+// app.use(cors());
 
 // ADICIONAR ENV
-mongoose.connect(
-  "mongodb+srv://admin:vitale123@cluster0-uatkq.mongodb.net/test?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+// mongoose.connect(
+//   "mongodb+srv://admin:vitale123@cluster0-uatkq.mongodb.net/test?retryWrites=true&w=majority",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   }
+// );
 
-mongoose.connection.once("open", () => {
-  console.log("Connected to database");
-});
-app.get("/", (req, res) => res.send("Hello World!"));
-app.use("/auth", auth);
+// mongoose.connection.once("open", () => {
+//   console.log("Connected to database");
+// });
+// app.get("/", (req, res) => res.send("Hello World!"));
+// app.use("/auth", auth);
 
-// GraphQL Middleware
-// Usamos o graphqlHTTP para o express entender o GraphQL
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    graphiql: true
-  })
-);
-
-// Usando nodemon para manter o server atualizando
-// npm i -g nodemon
-app.listen(4000, () => {
-  console.log("Now listening for requests on port " + process.env.PORT || 4000);
-});
+// // GraphQL Middleware
+// // Usamos o graphqlHTTP para o express entender o GraphQL
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     schema,
+//     graphiql: true
+//   })
+// );
+console.log("TESTE");
+// // Usando nodemon para manter o server atualizando
+// // npm i -g nodemon
+app.listen(process.env.PORT);
